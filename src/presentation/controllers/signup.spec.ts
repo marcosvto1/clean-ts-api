565c3b029421b3e5bdbca84d34d3627fd71dd34d
Marcos Tomaz <marcosvto1@gmail.co>
2020-09-12 21:49:33 -0300

    refactor: change mocking stragy

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -150,4 +150,26 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new ServerError());
 
   });
+
+  test('Should return 500 if EmailValidator throws with jest', () => {
+    const { sut, emailValidatorStub} = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(
+      () => {
+        throw new Error();
+      }
+    )
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+
+  });
 })
