b2a12b2ba07562c41aa1dec6fe88644ef33102db
Marcos Tomaz <marcosvto1@gmail.co>
2020-09-14 13:42:05 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,6 +11,11 @@ describe('Account Mongo Respository', () => {
     await MongoHelper.disconnect();
   });
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  })
+
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository();
   }
