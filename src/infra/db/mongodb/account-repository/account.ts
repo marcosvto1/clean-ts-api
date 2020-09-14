fb566143527fd9114041fac9e01b9c90bbac7fc9
Marcos Tomaz <marcosvto1@gmail.co>
2020-09-14 13:48:00 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,16 +3,11 @@ import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
+
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
-    const account = this.convertToAccountModel(result.ops[0]);
-    return account;
-  }
-
-  private convertToAccountModel(accountResultMongo): AccountModel {
-    const {_id, ...accountWithoutId} = accountResultMongo;
-    return Object.assign({}, accountWithoutId, {id: _id});
+    return MongoHelper.map(result.ops[0]);
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,5 @@
 import { MongoClient, Collection } from 'mongodb';
+import { AccountModel } from '../../../../domain/models/account';
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -15,6 +16,12 @@ export const MongoHelper = {
 
   getCollection(name: string): Collection {
     return this.client.db().collection(name);
+  },
+
+  map: (collection: any): any => {
+    const {_id, ...collectionWithoutId} = collection;
+    return Object.assign({}, collectionWithoutId, {id: _id});
   }
+
 
 }
