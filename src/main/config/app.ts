b8a5cb4fc12ecfa0cfbc518f3b93294cd6e9e038
Marcos Tomaz <marcosvto1@gmail.co>
2020-09-15 21:43:22 -0300

    feat: add signup routes integrations tests

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import setupMiddlwares from './middlewares';
+import setupRoutes from './routes';
 
 const app = express();
 setupMiddlwares(app);
+setupRoutes(app);
 
 export default app;
