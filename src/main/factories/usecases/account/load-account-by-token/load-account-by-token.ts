7f79bf4d4eacc0f7fbaa76fc4810248680df7b71
Marcos Tomaz <marcosvto1@gmail.co>
2020-11-15 21:06:21 -0300

    feat: ensure add survey route returns 403 if no acess token is provided

---
+++ src/main/factories/usecases/account/load-account-by-token/load-account-by-token.ts
@@ -0,0 +1,11 @@
+import { AccountMongoRepository } from './../../../../../infra/db/mongodb/account/account-mongo-repository';
+import { JwtAdapter } from './../../../../../infra/criptography/jwt-adapter/jwt-adapter';
+import { DbLoadAccountByToken } from './../../../../../data/usecases/load-account-by-token/db-load-account-by-token';
+import { LoadAccountByToken } from '../../../../../domain/usecases/load-account-by-token';
+import env from '../../../../config/env';
+
+export const makeLoadAccountByToken = (): LoadAccountByToken => {
+  const jwtDecrypter = new JwtAdapter(env.jwtSecret);
+  const accountRespository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(jwtDecrypter, accountRespository);
+}
--- src/main/routes/survey.routes.ts
+++ src/main/routes/survey.routes.ts
@@ -1,7 +1,10 @@
+import { adaptMiddleware } from './../adpters/express-middleware-adapter';
+import { makeAuthMiddleware } from './../factories/middlewares/auth-middleware-factory';
 import { Router } from 'express';
 import { adaptRoute } from '../adpters/express-route-adapter';
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory';
 
 export default (router: Router): void => {
-  router.post('/surveys', adaptRoute(makeAddSurveyController()));
+  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'));
+  router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()));
 } 
