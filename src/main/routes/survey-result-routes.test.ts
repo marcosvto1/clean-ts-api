6fdb9a19361e60d3f1ae3dc787d961d91292722a
Marcos <marcosvto1@gmail.com>
2020-12-27 21:59:01 -0300

    test: ensure save survey route return 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,3 +1,4 @@
+import { AccountMongoRepository } from './../../infra/db/mongodb/account/account-mongo-repository';
 import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
@@ -7,6 +8,25 @@ import env from '../config/env';
 
 let surveyCollection: Collection;
 let accountCollection: Collection;
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Marcos',
+    email: 'marcosvto1@gmail.com',
+    password: '123'
+  });
+
+  const id = res.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken;
+}
 
 describe('Survey Routes', () => {
 
@@ -29,12 +49,37 @@ describe('Survey Routes', () => {
   describe('PUT /surveys/:surveyId/results', () => {
     test('Should return 403 on save survey without accessToken', async () => {
       await request(app)
-        .put(`/api/surveys/1/results`)
+        .put(`/api/surveys/any_id/results`)
         .send({
           answer: 'any_answer'
         })
         .expect(403);
     });
+
+    test('Should return 200 on save survey with accessToken', async () => {
+      const accessToken = await makeAccessToken();
+      const res = await surveyCollection.insertOne({
+        question: 'Question 1',
+        answers: [
+          {
+            answer: 'Answer 11',
+            image: ''
+          },
+          {
+            answer: 'Answer 21',
+          }
+        ],
+        date: new Date()
+      });
+      await request(app)
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 11'
+        }).expect(200)
+
+    });
+    
 
   });
 
